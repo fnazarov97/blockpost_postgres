a9184af960ea15d76be314488d9cb5e32192f90b
Farhod Nazarov <farxodnazarov0728@gmail.com>
2022-11-20 10:27:20 +0500

    added env.exp

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -9,9 +9,7 @@ type Postgres struct {
 	DB *sqlx.DB
 }
 
-const auth = "user=farhod password=f@rhod666997 dbname=blockpost sslmode=disable"
-
-func InitDB() (p *Postgres, err error) {
+func InitDB(auth string) (p *Postgres, err error) {
 	db, err := sqlx.Connect("postgres", auth)
 	if err != nil {
 		return p, err
